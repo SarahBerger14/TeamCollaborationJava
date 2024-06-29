482b609b6cc36e8c82df5df1e7e865f0d5d078ad
vanessabieba <s52701@edu.campus02.at>
2024-06-29 12:08:06 +0200

    drinkqueue modified

--- src/main/java/at/campus02/bsd/DrinkQueue.java
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -1,6 +1,15 @@
 package at.campus02.bsd;
 
+import java.util.ArrayList;
+
+
 public class DrinkQueue implements IQueue{
+    private ArrayList<String> drinkQueue;
+
+    public DrinkQueue() {
+        drinkQueue = new ArrayList<String>();
+    }
+
     @Override
     public boolean offer(String obj) {
         return false;
