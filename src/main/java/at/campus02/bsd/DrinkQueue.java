6963ca7f820f51863ec2d132c865d8bbf8a9c8e1
vanessabieba <s52701@edu.campus02.at>
2024-06-29 11:46:15 +0200

    create class drinkqueue

---
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -0,0 +1,28 @@
+package at.campus02.bsd;
+
+public class DrinkQueue implements IQueue{
+    @Override
+    public boolean offer(String obj) {
+        return false;
+    }
+
+    @Override
+    public String poll() {
+        return "";
+    }
+
+    @Override
+    public String remove() {
+        return "";
+    }
+
+    @Override
+    public String peek() {
+        return "";
+    }
+
+    @Override
+    public String element() {
+        return "";
+    }
+}
